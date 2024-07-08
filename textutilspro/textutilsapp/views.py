11ebaf1e13a96972d600fcfa2ace79b28a949f37
Parth Sharma <117350765+parth-sharma1@users.noreply.github.com>
2024-07-08 07:05:25 +0000

    done extraspaceremover, newlineremover, fullcaps.......

--- textutilspro/textutilsapp/views.py
+++ textutilspro/textutilsapp/views.py
@@ -10,10 +10,17 @@ def index(request):
 def analyze(request):
     # get the text
     text = (request.GET.get("textarea"))
-    removepunc = (request.GET.get("removepunc", "off"))
-    print(removepunc)
-    print(text)
 
+    # check the checkboxes values
+    removepunc = (request.GET.get("removepunc", "off"))
+    fullcaps = (request.GET.get("fullcaps", "off"))
+    newlineremover = (request.GET.get("newlineremover", "off"))
+    extraspaceremover = (request.GET.get("extraspaceremover", "off"))
+
+   
+
+
+    # check which checkbox is on
     if removepunc == "on":
     
         punctuations = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
@@ -21,14 +28,64 @@ def analyze(request):
         for char in text:
             if char not in punctuations:
                 analyzed = analyzed+char
-
+    
 
         params = {'purpose':'removed punctuations', "analysed_text":analyzed}
 
         # analyze the text
         return render(request, "analyze.html", params)
+    
+
+    # checking the uppercase checkbox
+    elif (fullcaps=="on"):
+        analyzed=""
+        for char in text:
+            analyzed+=char.upper()
+        
+        params = {'purpose':'changed to uppercase', "analysed_text":analyzed}
+
+        # analyze the text
+        return render(request, "analyze.html", params)
+    
+
+
+    # checking the new Line remover checkbox
+    elif (newlineremover=="on"):
+        analyzed= ""
+        for char in text:
+            if char !="\n":
+                analyzed+=char
+            
+        params = {'purpose':'Removed NewLines', "analysed_text":analyzed}
+        return render(request, "analyze.html", params)
+
+
+    # checking the extra space remover checkbox
+    elif(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(text):
+            if text[index] == " " and text[index+1] == " ":
+                pass
+
+            else:
+                analyzed+=char
+
+        params = {'purpose':'Extraspaceremover', "analysed_text":analyzed}
+        return render(request, "analyze.html", params)
+
+
     else:
         return HttpResponse("Error")
+
+
+
+
+
+
+
+
+
+
 
 
 # def capitalizefirst(request):
@@ -45,5 +102,3 @@ def analyze(request):
 
 # def charcount(request):
 #     return HttpResponse("Hello this is an charactercount page")
-
-
