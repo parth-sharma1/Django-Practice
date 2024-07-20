d01e5656da7fea1ae85ee42590d765693d94d561
Parth Sharma <117350765+parth-sharma1@users.noreply.github.com>
2024-07-20 05:40:28 +0000

    edit part is completed

--- crudpro/crudapp/views.py
+++ crudpro/crudapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
 
 # admin-----> 1234
@@ -10,6 +10,38 @@ def index(request):
     context = {"data":data}
     print(context)
     return render(request, "index.html", context)
+
+
+
+def updateData(request, id):
+    if request.method=="POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age =  request.POST.get("age")
+        gender = request.POST.get("gender")
+
+        edit = Student.objects.get(id= id)
+        edit.name = name
+        edit.email = email
+        edit.gender = gender
+        edit.age = age
+        edit.save()
+        return redirect("/")
+    
+    d = Student.objects.get(id= id)
+    context = {"d":d}   
+    return render(request, "edit.html", context)
+
+
+
+
+
+def deleteData(request, id):
+    data = Student.objects.all()
+    context = {"data":data}
+    return render(request, "index.html", context)
+
+
 
 
 
@@ -26,4 +58,5 @@ def insertData(request):
         print(name, email, age, gender)
         query = Student(name = name, email = email, age=age, gender = gender)
         query.save()
+        return redirect("/")
     return render(request, "index.html")
